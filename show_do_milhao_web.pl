e12fa829902e05be140030aa05773b5c1f474c7d
AnaBeatrizMassuh <aaamassuh@gmail.com>
2024-07-16 21:32:33 -0300

    Correção na funcionalidade para passar pergunta e adicionando a funcionalidae de  pular

--- show_do_milhao_web.pl
+++ show_do_milhao_web.pl
@@ -1,17 +1,16 @@
-% arquivo principal
-
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
 :- use_module(library(http/http_json)).
 :- use_module(library(http/http_files)).
 :- use_module(library(http/html_write)).
 :- use_module(library(random)).
-:- consult('perguntas.pl').  % Adicione esta linha para carregar o arquivo de perguntas
+:- consult('perguntas.pl').
 
 :- dynamic jogador/1.
 :- dynamic pontuacao/1.
 :- dynamic valor_acumulado/1.
 :- dynamic perguntas_em_ordem/1.
+:- dynamic pulou/1.
 
 % Servidor Web
 server(Port) :-
@@ -24,6 +23,7 @@ server(Port) :-
 :- http_handler(root(api/ajuda), fornecer_ajuda, []).
 :- http_handler(root(api/dica), fornecer_dica, []).
 :- http_handler(root(api/desistir), desistir, []).
+:- http_handler(root(api/pular), pular_pergunta, []).
 
 % Iniciar o jogo
 iniciar_jogo(_Request) :-
@@ -31,8 +31,10 @@ iniciar_jogo(_Request) :-
     retractall(jogador(_)),
     retractall(pontuacao(_)),
     retractall(valor_acumulado(_)),
+    retractall(pulou(_)),
     assertz(pontuacao(0)),
     assertz(valor_acumulado(0)),
+    assertz(pulou(false)),
     reply_json(_{status: 'Jogo iniciado'}).
 
 % Enviar pergunta
@@ -52,23 +54,25 @@ enviar_pergunta(_Request) :-
 verificar_resposta(Request) :-
     catch(
         (   http_read_json_dict(Request, Dados),
-            atom_string(AtomNumero, Dados.numero), % Converte para átomo
-            atom_string(AtomResposta, Dados.resposta), % Converte para átomo
-            atom_number(AtomNumero, Numero), % Converte átomo para número
+            Numero is Dados.numero, % Diretamente convertendo para número
+            atom_string(Dados.resposta, Resposta),
+            format(user_error, 'Recebido: numero=~w, resposta=~w~n', [Numero, Resposta]),
             perguntas_em_ordem(PerguntasEmbaralhadas),
             nth1(Numero, PerguntasEmbaralhadas, pergunta(_, _, _, RespostaCorreta, _)),
-            (AtomResposta = RespostaCorreta ->
+            atom_string(RespostaCorreta, RespostaCorretaString),
+            atom_string(Resposta, RespostaString),
+            format(user_error, 'Resposta correta: ~w~n', [RespostaCorretaString]),
+            (RespostaString == RespostaCorretaString ->
                 incrementar_pontuacao,
                 valor_acumulado(ValorAcumulado),
                 reply_json(_{status: 'Correto', valor_acumulado: ValorAcumulado});
-                reply_json(_{status: 'Errado', resposta_correta: RespostaCorreta}))
+                reply_json(_{status: 'Errado', resposta_correta: RespostaCorretaString}))
         ),
         Error,
         (   format(user_error, 'Erro ao processar requisição POST para /api/resposta: ~w~n', [Error]),
             reply_json(_{error: 'Erro interno do servidor'}, [status(500)])
         )
     ).
-
 
 % Incrementar pontuação e valor acumulado
 incrementar_pontuacao :-
@@ -80,6 +84,27 @@ incrementar_pontuacao :-
     NovoV is V + 65000,
     retract(valor_acumulado(V)),
     assertz(valor_acumulado(NovoV)).
+
+% Enviar próxima pergunta
+enviar_proxima_pergunta :-
+    pontuacao(P),
+    perguntas_em_ordem(PerguntasEmbaralhadas),
+    length(PerguntasEmbaralhadas, NumPerguntas),
+    (P < NumPerguntas ->
+        nth0(P, PerguntasEmbaralhadas, pergunta(_, Pergunta, Alternativas, _, Dica)),
+        ProximaPergunta is P + 1,
+        (ProximaPergunta mod 5 =:= 0 ->
+            reply_json(_{numero: ProximaPergunta, pergunta: Pergunta, alternativas: Alternativas, dica: Dica, especial: true});
+            reply_json(_{numero: ProximaPergunta, pergunta: Pergunta, alternativas: Alternativas, dica: Dica}));
+        finalizar_jogo).
+
+% Pular pergunta
+pular_pergunta(_Request) :-
+    pulou(false),
+    incrementar_pontuacao,
+    retract(pulou(false)),
+    assertz(pulou(true)),
+    enviar_proxima_pergunta.
 
 % Finalizar o jogo
 finalizar_jogo :-
@@ -96,8 +121,7 @@ desistir(_Request) :-
 fornecer_dica(Request) :-
     catch(
         (   http_read_json_dict(Request, Dados),
-            atom_string(AtomNumero, Dados.numero), % Converte para átomo
-            atom_number(AtomNumero, Numero), % Converte átomo para número
+            Numero is Dados.numero, % Diretamente convertendo para número
             perguntas_em_ordem(PerguntasEmbaralhadas),
             nth1(Numero, PerguntasEmbaralhadas, pergunta(_, _, _, _, Dica)),
             reply_json(_{dica: Dica})
@@ -118,7 +142,6 @@ server_loop :-
 :- initialization(server(8080)),
    initialization(server_loop).
 
-:- http_handler(root(api/ajuda), fornecer_ajuda, []).
+% Fornecer ajuda
 fornecer_ajuda(_Request) :-
     reply_json(_{mensagem: 'Este é o Show do Milhão! Responda as perguntas corretamente e ganhe pontos. Clique na alternativa correta para responder.'}).
-
